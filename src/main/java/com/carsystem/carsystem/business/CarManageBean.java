3396157150bd1c9131b2cfaa953c8303863b8ea3
Gabriel <contato.gabrielgarcia@gmail.com>
2018-11-22 10:44:40 -0200

    [FIX] Add some features to the project

--- src/main/java/com/carsystem/carsystem/business/CarManageBean.java
+++ src/main/java/com/carsystem/carsystem/business/CarManageBean.java
@@ -1,13 +1,44 @@
 package com.carsystem.carsystem.business;
 
+import com.carsystem.carsystem.persistence.entity.Car;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.ManagedBean;
+import java.util.ArrayList;
+import java.util.List;
 
 @Scope(value = "session")
 @ManagedBean(value = "carManageBean")
 public class CarManageBean {
+
+    private Car car;
+    private List<Car> cars= new ArrayList<>();
+
+    /**
+     * Method responsable to add car on the list
+     */
+    @Autowired
+    public void add(){
+        cars.add(car);
+        car = new Car();
+    }
+
+    public Car getCar() {
+        return car;
+    }
+
+    public void setCar(Car car) {
+        this.car = car;
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public void setCars(List<Car> cars) {
+        this.cars = cars;
+    }
 
     @Autowired
     public String loadTodoPage() {
---
+++ src/main/java/com/carsystem/carsystem/persistence/entity/Car.java
@@ -0,0 +1,55 @@
+package com.carsystem.carsystem.persistence.entity;
+
+import org.hibernate.annotations.Entity;
+import org.springframework.data.annotation.Id;
+
+@Entity
+public class Car {
+
+    @Id
+    private Integer id;
+    private String modelName;
+    private String brand;
+    private String color;
+    private Integer year;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+}
